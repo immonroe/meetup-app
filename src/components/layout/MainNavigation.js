4a13f6c6c35f048f7becc113f7dd9ecbba1b56a7
immonroe <immanuelmonroe@gmail.com>
2023-07-25 19:47:55 -0400

    Fixed bug with routing and added nav links

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function MainNavigation() {
+    return <headers>
+        <div>React Meetups</div>
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>All Meetups</Link>
+                </li>
+                <li>
+                    <Link to='/new-meetup'>Add New Meetup</Link>
+                </li>
+                <li>
+                    <Link to='/favorites'>My Favorites</Link>
+                </li>
+            </ul>
+        </nav>
+    </headers>
+}
+
+export default MainNavigation;
